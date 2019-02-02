2617d7be8056d78554571cfeefb85e3346dab4de
vijaygovind <vijucg@yahoo.com>
2019-02-02 10:24:44 +0530

    My first commit of Opstools and pipleline setup
    Signed-off-by: vijaygovind <vijucg@yahoo.com>

---
+++ Opstools/src/main/java/com/jpmc/H2H/Opstools/OpstoolsApplication.java
@@ -0,0 +1,14 @@
+package com.jpmc.H2H.Opstools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpstoolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpstoolsApplication.class, args);
+	}
+
+}
+
---
+++ Opstools/src/test/java/com/jpmc/H2H/Opstools/OpstoolsApplicationTests.java
@@ -0,0 +1,17 @@
+package com.jpmc.H2H.Opstools;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OpstoolsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
