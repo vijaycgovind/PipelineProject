fad57535d67a75f103c69bbaffb6bf15e702dd58
vijaygovind <vijucg@yahoo.com>
2019-02-02 14:19:06 +0530

    testg Signed-off-by: vijaygovind <vijucg@yahoo.com>

--- Opstools/src/main/java/com/jpmc/H2H/Opstools/OpstoolsApplication.java
+++ Opstools/src/main/java/com/jpmc/H2H/Opstools/OpstoolsApplication.java
@@ -9,8 +9,7 @@ public class OpstoolsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(OpstoolsApplication.class, args);
 		System.out.println("Jai Vinayaka");
-		System.out.println("Jai Hanuman");
-		
+		System.out.println("Jai Hanuman");		
 	}
 
 }
